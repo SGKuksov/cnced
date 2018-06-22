067c53d62cb77e488e38dbba89f87fc56c99b85e
sgkuksov <sgkuksov1989@gmail.com>
2018-06-22 06:26:17 +0300

    Стилизовал promo, popular

--- src/js/script.js
+++ src/js/script.js
@@ -2,4 +2,19 @@ svg4everybody(); // иницализация полифила для IE
 
 $(document).ready(function() {
 
+  $(".promo__card-point").click(function() {
+    $(".promo__card-baloon").slideToggle();
+    $(".promo__card-baloon").slideToggle();
+  });
+
+
+
+
+
+
+
+
+
+
+
 });
